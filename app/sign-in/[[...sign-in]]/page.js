8a734bdce95816bb9a2dfaa4b809f796098d0489
Jaehoonoo <justjhoon@gmail.com>
2024-08-20 18:03:01 -0400

    sign-in page is now mobile responsive.

--- app/sign-in/[[...sign-in]]/page.js
+++ app/sign-in/[[...sign-in]]/page.js
@@ -8,21 +8,20 @@ import styles from './page.module.css'
 
 export default function SignUpPage() {
     return(
-    <Container disableGutters sx={{minWidth: '100vw'}}>
-        <AppBar position="static">
+    <Container disableGutters className={styles.container} sx={{minWidth: '100vw', minHeight: '100vh'}}>
+        <AppBar position="absolute">
             <Toolbar sx={{bgcolor: 'white', padding:"8px"}}>
                 <Image src="/assets/cardlet-logo.png" alt="Logo" width={125} height={50} />
             </Toolbar>
         </AppBar>
-        <div className={styles.container}>
-            <Box
-                display="flex"
-                flexDirection="column"
-                alignItems="center"
-                justifyContent="center">
-                <SignIn signUpUrl='/sign-up' />
-            </Box>
-        </div>
+
+        <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center">
+            <SignIn signUpUrl='/sign-up' />
+        </Box>
   </Container>
     )
   
--- app/sign-up/[[...sign-up]]/page.js
+++ app/sign-up/[[...sign-up]]/page.js
@@ -8,22 +8,20 @@ import styles from './page.module.css'
 
 export default function SignUpPage() {
     return(
-    <Container disableGutters sx={{minWidth: '100vw'}}>
-        <AppBar position="static" sx={{backgroundColor: '#3f51b5'}}>
+    <Container disableGutters className={styles.container} sx={{minWidth: '100vw', minHeight: '100vh'}}>
+        <AppBar position="absolute" sx={{backgroundColor: '#3f51b5'}}>
             <Toolbar sx={{bgcolor: 'white', padding:"8px"}}>
                 <Image src="/assets/cardlet-logo.png" alt="Logo" width={125} height={50} />
             </Toolbar>
         </AppBar>
 
-        <div className={styles.container}>
-            <Box
-                display="flex"
-                flexDirection="column"
-                alignItems="center"
-                justifyContent="center">
-                <SignUp signInUrl='/sign-in' />
-            </Box>
-        </div>
+        <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center">
+            <SignUp signInUrl='/sign-in' />
+        </Box>
   </Container>
     )
   
