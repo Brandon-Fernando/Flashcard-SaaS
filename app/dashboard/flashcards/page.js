57198b70c1d92f6574a3a9a7c03c624127130715
Jaehoonoo <justjhoon@gmail.com>
2024-08-20 00:29:02 -0400

    Small bug fixes.

--- app/dashboard/flashcards/page.js
+++ app/dashboard/flashcards/page.js
@@ -273,7 +273,7 @@ export default function Flashcards() {
     }
 
     const handleCardClick = (id) => {
-        router.push(`/flashcard?id=${id}`);
+        router.push(`/dashboard/flashcard?id=${id}`);
     };
 
     const handleMenuClick = (event, id) => {
